997f2f6c315abb7f61321bae76c97b29fd3a57f8
Miksi <ivanovicmihajlo140@gmail.com>
2024-06-03 19:30:50 +0200

    FInalni commit

---
+++ B02/B02/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿document.getElementById("curka").addEventListener("mouseover", function () { new Audio('./zvukovi/curka.mp3').play() });
+document.getElementById("jagnje").addEventListener("mouseover", function () { new Audio('./zvukovi/jagnje.mp3').play() });
+document.getElementById("kokoska").addEventListener("mouseover", function () { new Audio('./zvukovi/kokoska.mp3').play() });
+document.getElementById("pas").addEventListener("mouseover", function () { new Audio('./zvukovi/pas.mp3').play() });
+document.getElementById("svinja").addEventListener("mouseover", function () { new Audio('./zvukovi/svinja.mp3').play() });
+
+document.getElementById("curka").addEventListener("click", function () { alert("ovo je jenda od zivotinja ikada") });
+document.getElementById("jagnje").addEventListener("click", function () { alert("ovo je jenda od zivotinja ikada") });
+document.getElementById("kokoska").addEventListener("click", function () { alert("ovo je jenda od zivotinja ikada") });
+document.getElementById("pas").addEventListener("click", function () { alert("ovo je jenda od zivotinja ikada") });
+document.getElementById("svinja").addEventListener("click", function () { alert("ovo je jenda od zivotinja ikada") });
